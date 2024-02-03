8f423966a34d60363445aea89072765e548e20e8
Maciek Skokowski <maciejskok2004@gmail.com>
2024-02-03 01:47:17 +0100

    fixed responsive of reg and log style to last commit

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,58 +10,56 @@ const Login: React.FC = () => {
 
 	return (
 		<div className='space-dark-reg-log'>
-			<div className='content-full-space-centered'>
-				<div className='reg-log-container'>
-					<Link href='/' className='reg-log-brand-text'>
-						VVACIEJ.APP
-					</Link>
-					<div className='reg-log-box'>
-						<h1>Zaloguj się na swoje konto</h1>
-						<section className='reg-log-input-form'>
-							<section>
-								<label htmlFor='email'>Email</label> <input type='text' id='email' />
-							</section>
-							<section>
-								<section className='flex justify-between'>
-									<label htmlFor='password'>Hasło</label>
-									<Link href='/forgot-password' className='orange-link'>
-										Zapomniałeś hasła?
-									</Link>
-								</section>
-								<input type='text' id='password' />
-								<section className='flex items-center gap-x-2 mt-2'>
-									<input type='checkbox' id='remember' className='orange-checkbox' />
-									<label htmlFor='remember' className='select-none'>
-										Zapamiętaj
-									</label>
-								</section>
-							</section>
-						</section>
-						<button className='reg-log-create-acc-btn'>Kontynuuj</button>
-						<section className='reg-log-authorize-section'>
-							<span className='login-authorize-text'>
-								Lub zaloguj się przez <hr></hr>
-							</span>
-							<button className='reg-log-google-authorize-btn'>
-								<img
-									src='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png'
-									className='h-10'
-									alt='google-icon'
-								/>
-							</button>
-						</section>
-					</div>
-					<p>
-						Don&apos;t have an account?&nbsp;
-						<Link href='/register' className='orange-link'>
-							Sign up
-						</Link>
-					</p>
-				</div>
-				<Link href='/' className='reg-log-bottom-link'>
-					&copy; src obejrzyj.to
+			<div className='reg-log-container'>
+				<Link href='/' className='reg-log-brand-text'>
+					VVACIEJ.APP
 				</Link>
+				<div className='reg-log-box'>
+					<h1>Zaloguj się na swoje konto</h1>
+					<section className='reg-log-input-form'>
+						<section>
+							<label htmlFor='email'>Email</label> <input type='text' id='email' />
+						</section>
+						<section>
+							<section className='flex justify-between'>
+								<label htmlFor='password'>Hasło</label>
+								<Link href='/forgot-password' className='orange-link'>
+									Zapomniałeś hasła?
+								</Link>
+							</section>
+							<input type='text' id='password' />
+							<section className='flex items-center gap-x-2 mt-2'>
+								<input type='checkbox' id='remember' className='orange-checkbox' />
+								<label htmlFor='remember' className='select-none'>
+									Zapamiętaj
+								</label>
+							</section>
+						</section>
+					</section>
+					<button className='reg-log-create-acc-btn'>Kontynuuj</button>
+					<section className='reg-log-authorize-section'>
+						<span className='login-authorize-text'>
+							Lub zaloguj się przez <hr></hr>
+						</span>
+						<button className='reg-log-google-authorize-btn'>
+							<img
+								src='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png'
+								className='h-10'
+								alt='google-icon'
+							/>
+						</button>
+					</section>
+				</div>
+				<p>
+					Don&apos;t have an account?&nbsp;
+					<Link href='/register' className='orange-link'>
+						Sign up
+					</Link>
+				</p>
 			</div>
+			<Link href='/' className='reg-log-bottom-link'>
+				&copy; src obejrzyj.to
+			</Link>
 		</div>
 	);
 };
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,50 +10,48 @@ const Policy: React.FC = () => {
 
 	return (
 		<div className='space-dark-reg-log'>
-			<div className='content-full-space-centered'>
-				<div className='reg-log-container'>
-					<Link href='/' className='reg-log-brand-text'>
-						VVACIEJ.APP
-					</Link>
-					<div className='reg-log-box'>
-						<h1>Stwórz nowe konto</h1>
-						<section className='reg-log-input-form'>
-							<section>
-								<label htmlFor='email'>Email</label>
-								<input type='text' id='email' />
-							</section>
-							<section>
-								<label htmlFor='password'>Hasło</label>
-								<input type='text' id='password' />
-							</section>
-							<section>
-								<label htmlFor='confirm-password'>Potwierdź hasło</label>
-								<input type='text' id='confirm-password' />
-							</section>
-						</section>
-						<button className='reg-log-create-acc-btn'>Stwórz konto</button>
-						<section className='reg-log-authorize-section'>
-							<span className='register-authorize-text'>
-								Lub zajerestruj się przez
-								<hr></hr>
-							</span>
-							<button className='reg-log-google-authorize-btn'>
-								<img
-									src='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png '
-									className='h-10'
-									alt='google-icon'
-								/>
-							</button>
-						</section>
-					</div>
-					<p>
-						Already have an account? <Link href='/login' className='orange-link'>Sign in</Link>
-					</p>
-				</div>
-				<Link href='/' className='reg-log-bottom-link'>
-					&copy; src obejrzyj.to
+			<div className='reg-log-container'>
+				<Link href='/' className='reg-log-brand-text'>
+					VVACIEJ.APP
 				</Link>
+				<div className='reg-log-box'>
+					<h1>Stwórz nowe konto</h1>
+					<section className='reg-log-input-form'>
+						<section>
+							<label htmlFor='email'>Email</label>
+							<input type='text' id='email' />
+						</section>
+						<section>
+							<label htmlFor='password'>Hasło</label>
+							<input type='text' id='password' />
+						</section>
+						<section>
+							<label htmlFor='confirm-password'>Potwierdź hasło</label>
+							<input type='text' id='confirm-password' />
+						</section>
+					</section>
+					<button className='reg-log-create-acc-btn'>Stwórz konto</button>
+					<section className='reg-log-authorize-section'>
+						<span className='register-authorize-text'>
+							Lub zajerestruj się przez
+							<hr></hr>
+						</span>
+						<button className='reg-log-google-authorize-btn'>
+							<img
+								src='https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png '
+								className='h-10'
+								alt='google-icon'
+							/>
+						</button>
+					</section>
+				</div>
+				<p>
+					Already have an account? <Link href='/login' className='orange-link'>Sign in</Link>
+				</p>
 			</div>
+			<Link href='/' className='reg-log-bottom-link'>
+				&copy; src obejrzyj.to
+			</Link>
 		</div>
 	);
 };
