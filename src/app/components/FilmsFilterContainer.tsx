1f9fe9a479bc4cbe1c813bbb5c72d03d60bd1fd6
Maciek Skokowski <maciejskok2004@gmail.com>
2024-02-12 23:48:25 +0100

    fixed styling footer dropdown lang

--- src/app/components/FilmsFilterContainer.tsx
+++ src/app/components/FilmsFilterContainer.tsx
@@ -431,9 +431,7 @@ const Filters: React.FC<FilterPageProps> = ({ headingTitlePage, mappingBy }) => 
 				</div>
 			</div>
 			<Footer />
-			<div ref={mostPopularDropdownMobilesRef} className={`typical-dropdown-style ${mostPopularBtnClicked ? 'active' : ''}`} style={{
-				height: '11rem !important',
-			}}>
+			<div ref={mostPopularDropdownMobilesRef} className={`typical-dropdown-style ${mostPopularBtnClicked ? 'active' : ''} !h-48`}>
 				{mostPopularBtnClicked ? (
 					<div>
 						<button
