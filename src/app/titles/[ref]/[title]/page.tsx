ddf0336b5fecf93ea9d9bd9545b145ba81b7905c
Maciek Skokowski <maciejskok2004@gmail.com>
2024-02-17 02:58:09 +0100

    fixed style for last commit

--- src/app/titles/[ref]/[title]/page.tsx
+++ src/app/titles/[ref]/[title]/page.tsx
@@ -34,6 +34,14 @@ interface pageProps {
 }
 
 const FilmPage: React.FC<pageProps> = ({ params }) => {
+	const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoaded(true);
+		}, 300);
+	}, []);
+
 	const infoOfChoosedFilm = allFilmsData.find(film => convertTitleToUrl(film.title) === params.title);
 
 	const extractYearFromReleaseDate = (releaseDate: string | undefined) => {
@@ -62,94 +70,102 @@ const FilmPage: React.FC<pageProps> = ({ params }) => {
 			{isFilmExist ? (
 				<div className='space-light'>
 					<Navbar isCutted={false} />
-					<div className='film-page-image-fullhd-preview'>
-						<button className='film-play-btn'>
-							<PlayIcon className='text-black h-6' />
-						</button>
-						<div className='film-page-image-gradient'></div>
-						<div className='film-page-image-blured'></div>
-						<img src={infoOfChoosedFilm?.imgFullHd1280} alt={`Image for ${infoOfChoosedFilm?.title}`} />
-						<div className='relative'>
-							<img src={infoOfChoosedFilm?.imgFullHd1280} alt={`Image for ${infoOfChoosedFilm?.title}`} />
-						</div>
-					</div>
-					<div className='content-full-space-centered'>
-						<div className='film-page-container'>
-							<aside className='film-page-aside'>
-								<img src={infoOfChoosedFilm?.image} alt={`Poster for ${infoOfChoosedFilm?.title}`} />
-								<button className='orange-btn-style'>
-									<PlayIcon className='h-4' />
-									Obejrzyj to
+					{isLoaded ? (
+						<>
+							<div className='film-page-image-fullhd-preview'>
+								<button className='film-play-btn'>
+									<PlayIcon className='text-black h-6' />
 								</button>
-								<button className='orange-outlined-btn-style'>
-									<PlusIcon className='h-4' />
-									Obejrzyj potem
-								</button>
-								<button className='orange-outlined-btn-style'>
-									<ShareIcon className='h-4' />
-									Udostępnij
-								</button>
-								<section>
-									<b>Oryginalny język</b>
-									<span>---</span>
-								</section>
-								<section>
-									<b>Oryginalny tytuł</b>
-									<span>---</span>
-								</section>
-								<section>
-									<b>Budżet</b>
-									<span>
-										{infoOfChoosedFilm?.budget.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-									</span>
-								</section>
-								<section>
-									<b>Przychód</b>
-									<span>
-										{' '}
-										{infoOfChoosedFilm?.profit.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-									</span>
-								</section>
-							</aside>
-							<main className='film-page-main'>
-								<section className='film-page-main-top-info-text-section'>
-									<section className='film-page-main-top-info-heading-section'>
+								<div className='film-page-image-gradient'></div>
+								<div className='film-page-image-blured'></div>
+								<img src={infoOfChoosedFilm?.imgFullHd1280} alt={`Image for ${infoOfChoosedFilm?.title}`} />
+								<div className='relative'>
+									<img src={infoOfChoosedFilm?.imgFullHd1280} alt={`Image for ${infoOfChoosedFilm?.title}`} />
+								</div>
+							</div>
+							<div className='content-full-space-centered'>
+								<div className='film-page-container'>
+									<aside className='film-page-aside'>
+										<img src={infoOfChoosedFilm?.image} alt={`Poster for ${infoOfChoosedFilm?.title}`} />
+										<button className='orange-btn-style'>
+											<PlayIcon className='h-4' />
+											Obejrzyj to
+										</button>
+										<button className='orange-outlined-btn-style'>
+											<PlusIcon className='h-4' />
+											Obejrzyj potem
+										</button>
+										<button className='orange-outlined-btn-style'>
+											<ShareIcon className='h-4' />
+											Udostępnij
+										</button>
 										<section>
-											<h1 className='film-page-film-title'>{infoOfChoosedFilm?.title}</h1>
-											<section>
-												<span>{infoOfChoosedFilm?.releaseDate}</span>
-												<span className='ml-2 mr-2'>•</span>
-												<span>{infoOfChoosedFilm?.time}</span>
-											</section>
+											<b>Oryginalny język</b>
+											<span>---</span>
 										</section>
 										<section>
-											<section className='main-text-rating flex items-center min-w-max'>
-												<StarIcon
-													className='h-5 mr-2'
-													style={{
-														color: 'var(--orange)',
-													}}
-												/>
-												<span>{infoOfChoosedFilm?.rating} / 10</span>
-												<span
-													className='film-list-rating-separate-symbol ml-3'
-													style={{
-														color: 'var(--gray-5050)',
-													}}>
-													|
-												</span>
-												<button className='rate-this-btn flex items-center gap-x-2'>
-													<StarIcon className='h-4' />
-													Oceń to
-												</button>
-											</section>
+											<b>Oryginalny tytuł</b>
+											<span>---</span>
 										</section>
-									</section>
-									<p className='film-page-film-description'>{infoOfChoosedFilm?.description}</p>
-								</section>
-							</main>
+										<section>
+											<b>Budżet</b>
+											<span>
+												{infoOfChoosedFilm?.budget.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+											</span>
+										</section>
+										<section>
+											<b>Przychód</b>
+											<span>
+												{' '}
+												{infoOfChoosedFilm?.profit.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
+											</span>
+										</section>
+									</aside>
+									<main className='film-page-main'>
+										<section className='film-page-main-top-info-text-section'>
+											<section className='film-page-main-top-info-heading-section'>
+												<section>
+													<h1 className='film-page-film-title'>{infoOfChoosedFilm?.title}</h1>
+													<section>
+														<span>{infoOfChoosedFilm?.releaseDate}</span>
+														<span className='ml-2 mr-2'>•</span>
+														<span>{infoOfChoosedFilm?.time}</span>
+													</section>
+												</section>
+												<section>
+													<section className='main-text-rating flex items-center min-w-max'>
+														<StarIcon
+															className='h-5 mr-2'
+															style={{
+																color: 'var(--orange)',
+															}}
+														/>
+														<span>{infoOfChoosedFilm?.rating} / 10</span>
+														<span
+															className='film-list-rating-separate-symbol ml-3'
+															style={{
+																color: 'var(--gray-5050)',
+															}}>
+															|
+														</span>
+														<button className='rate-this-btn flex items-center gap-x-2'>
+															<StarIcon className='h-4' />
+															Oceń to
+														</button>
+													</section>
+												</section>
+											</section>
+											<p className='film-page-film-description'>{infoOfChoosedFilm?.description}</p>
+										</section>
+									</main>
+								</div>
+							</div>
+						</>
+					) : (
+						<div className='loader-container flex justify-center w-full h-full'>
+							<div className='loader'></div>
 						</div>
-					</div>
+					)}
 					<Footer />
 				</div>
 			) : (
