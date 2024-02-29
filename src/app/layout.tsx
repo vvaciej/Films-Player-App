11baf771f445d214a2445d2fcc58b3b1068c2dde
Maciek Skokowski <maciejskok2004@gmail.com>
2024-02-29 19:35:01 +0100

    changed meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,11 +54,11 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
 				<head>
 					<link rel='icon' href='/next.svg' />
 					<meta name='theme-color' content='#1a1a1a' />
-					<meta property='og:title' content={t('Free films and serials online!')} />
+					<meta property='og:title' content='Darmowe filmy i seriale online!' />
 					<meta property='og:image' content='/next.svg' />
 					<meta
 						name='description'
-						content={t('vvaciej.app is a popular database with movies and series fully free of charge')}
+						content='vvaciej.app is a popular database with movies and series fully free of charge'
 					/>
 					<meta
 						name='keywords'
